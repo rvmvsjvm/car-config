dadf882da39920721c992057f210eb32b382aa1b
Jaleel Akbar <jaleel.akbar@izmoltd.com>
2017-11-08 16:55:28 +0530

    adding src folder

---
+++ src/main/java/com/izmo/config/CarVisualizerConfigApplication.java
@@ -0,0 +1,12 @@
+package com.izmo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarVisualizerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarVisualizerConfigApplication.class, args);
+	}
+}
